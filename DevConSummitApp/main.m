8dd548f34ab60000661ca1832d308ddcb3c38b1b
Haifa Carina Baluyos <haifa@baluyos.net>
2014-09-28 02:35:50 +0800

    Initial commit from local git

---
+++ DevConSummitApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DevConSummitApp
+//
+//  Created by Haifa Carina Baluyos on 9/28/14.
+//  Copyright (c) 2014 HaifaCarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
