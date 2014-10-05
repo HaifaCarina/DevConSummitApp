3df2382d372983ff55d1d33b8fb09892d78b035a
Haifa Carina Baluyos <haifa@baluyos.net>
2014-10-06 03:22:37 +0800

    Add News Page

---
+++ DevConSummitApp/NewsViewController.m
@@ -0,0 +1,98 @@
+//
+//  NewsViewController.m
+//  DevConSummitApp
+//
+//  Created by Haifa Carina Baluyos on 10/5/14.
+//  Copyright (c) 2014 HaifaCarina. All rights reserved.
+//
+
+#import "NewsViewController.h"
+#import "SWRevealViewController.h"
+#import "NewsTableViewCell.h"
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromRGBWithAlpha(rgbValue,a) [UIColor \ colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \ green:((float)((rgbValue & 0xFF00) >> 8))/255.0 \ blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
+@interface NewsViewController () {
+    
+}
+@end
+
+@implementation NewsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    SWRevealViewController *revealController = [self revealViewController];
+    [self.navigationController.navigationBar addGestureRecognizer:revealController.panGestureRecognizer];
+    UIBarButtonItem *revealButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"navigation-icon.png"]
+                                                                         style:UIBarButtonItemStyleBordered target:revealController action:@selector(revealToggle:)];
+    self.navigationItem.leftBarButtonItem = revealButtonItem;
+    self.navigationItem.title = @"News";
+
+
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 5;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    static NSString *CellIdentifier = @"Cell";
+    NewsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil)
+    {
+        cell = [[NewsTableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:CellIdentifier];
+        
+    }
+    
+    
+    cell.header.text = @"Speakers";
+    cell.header.backgroundColor = UIColorFromRGB(0x6A4FFA);
+    
+    cell.textLabel.text = @"Meet our speaker from Philippine Android Developers Community!";
+    cell.detailTextLabel.text = @"The speaker is the founder of Philippine Android Developers Community.";
+    cell.imageView.image = [UIImage imageNamed:@"haifa.jpg"];
+    
+    
+    return cell;
+    
+    
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // NSLog(@"tableView willDisplayCell %@", indexPath);
+}
+
+#pragma mark - Table view delegate
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return indexPath;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    //  NSLog(@"did select %@", indexPath);
+    
+    
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    return 100;
+    
+    
+}
+
+
+
+@end
