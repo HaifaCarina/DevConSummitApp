baf3fd363738112e1236560bd8d6566ba0441934
Haifa Carina Baluyos <haifa@baluyos.net>
2014-10-06 03:20:58 +0800

    Add custom UITableViewCell for News page

---
+++ DevConSummitApp/NewsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewCell.h
+//  DevConSummitApp
+//
+//  Created by Haifa Carina Baluyos on 10/5/14.
+//  Copyright (c) 2014 HaifaCarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewCell : UITableViewCell
+
+@end
---
+++ DevConSummitApp/NewsTableViewCell.m
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewCell.m
+//  DevConSummitApp
+//
+//  Created by Haifa Carina Baluyos on 10/5/14.
+//  Copyright (c) 2014 HaifaCarina. All rights reserved.
+//
+
+#import "NewsTableViewCell.h"
+
+@implementation NewsTableViewCell
+
+@end
