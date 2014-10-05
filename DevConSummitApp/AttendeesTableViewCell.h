1977dfb8e1129680d70e31ade9239ff6b4f88c10
Haifa Carina Baluyos <haifa@baluyos.net>
2014-10-06 04:20:48 +0800

    Add custom UITableViewCell for attendees page

---
+++ DevConSummitApp/AttendeesTableViewCell.h
@@ -0,0 +1,12 @@
+//
+//  AttendeesTableViewCell.h
+//  DevConSummitApp
+//
+//  Created by Haifa Carina Baluyos on 10/6/14.
+//  Copyright (c) 2014 HaifaCarina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AttendeesTableViewCell : UITableViewCell
+@end
